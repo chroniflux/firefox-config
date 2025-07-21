b1b576e5fef49845764b787c2cb343d18f0f029a
jhxu <me@bitluna.me>
2025-07-21 16:06:41 -0400

    Enable disk cache, prefetch, and speculative connections

--- user_private.js
+++ user_private.js
@@ -1,3 +1,17 @@
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("browser.theme.native-theme", true);
 user_pref("browser.tabs.allow_transparent_browser", true);
+
+user_pref("browser.cache.disk.enable", true);
+user_pref("browser.sessionhistory.max_total_viewers", -1);
+user_pref("browser.contentblocking.category", "standard");
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.speculativeConnect.enabled", true);
+
+user_pref("network.dns.disablePrefetch", false);
+user_pref("network.dns.disablePrefetchFromHTTPS", false);
+user_pref("network.prefetch-next", true);
+user_pref("network.predictor.enabled", true);
+user_pref("network.predictor.enable-prefetch", true);
+user_pref("network.http.speculative-parallel-limit", 6);
+user_pref("network.http.pacing.requests.enabled", true);
