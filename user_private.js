13d6d9b5ca7aff1fa4833f1fb3721f34805e2e81
jhxu <me@bitluna.me>
2025-07-21 16:24:41 -0400

    Add blocklist support to user.js merge workflow
    Introduce user_block

--- user_private.js
+++ user_private.js
@@ -7,6 +7,7 @@ user_pref("browser.sessionhistory.max_total_viewers", -1);
 user_pref("browser.contentblocking.category", "standard");
 user_pref("browser.search.suggest.enabled", true);
 user_pref("browser.urlbar.speculativeConnect.enabled", true);
+user_pref("browser.safeBrowse.downloads.remote.enabled", true);
 
 user_pref("network.dns.disablePrefetch", false);
 user_pref("network.dns.disablePrefetchFromHTTPS", false);
@@ -15,3 +16,6 @@ user_pref("network.predictor.enabled", true);
 user_pref("network.predictor.enable-prefetch", true);
 user_pref("network.http.speculative-parallel-limit", 6);
 user_pref("network.http.pacing.requests.enabled", true);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 0);
+
+user_pref("security.OCSP.enabled", 1);
